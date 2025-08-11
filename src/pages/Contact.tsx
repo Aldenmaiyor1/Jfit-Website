266d0b8e4a7776de75a12eae34b1742962022ce2
aldenmaiyor1 <amai111@aucklanduni.ac.nz>
2025-08-12 02:29:51 +1200

    fixed map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const Contact = () => {
+const containerStyle = {
+    width: '100%',
+    height: '400px'
+};
+
+const center = { lat: -36.850127098234815, lng: 174.76459126009595 };
+
+
+function Contact() {
     return (
-        <div>
-            <div className='text-white'>
-                <p>Location 📍  : 239 Queens Street, Auckland CBD, 1010, Cityfitness</p>
-                <p>Phone number: 0211865231</p>
-                <p>Kakao ID : jino2239</p>
+        <div className="flex flex-col min-h-screen bg-black">
+            <div className='border-2 border-white p-4 rounded-lg bg-black bg-opacity-50 mx-8 my-4'>
+                <div className="text-white mb-4">
+                    <p>Location 📍 : 239 Queen Street, Auckland Central, 1010</p>
+                    <p>Phone number: 0211865231</p>
+                    <p>Kakao ID: jino2239</p>
+                </div>
+            </div>
+            <div className='mx-4'>
+                <LoadScript googleMapsApiKey="AIzaSyCge3KF9q0URCocNW1PwRA_uvlNG9pfHAc">
+                    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={18}>
+                        <Marker position={center} />
+                    </GoogleMap>
+                </LoadScript>
             </div>
 
-            <div>
-                <MapContainer
-                    center={[-36.8502, 174.7646]}
-                    zoom={13}
-                    style={{ height: '400px', width: '100%' }}
-                >
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution='© OpenStreetMap'
-                    />
-                </MapContainer>
-            </div>
         </div>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
